83c0f8d338cb7951852b33d4f7c7dd69f1c01377
rolen <rolen@kakao.com>
2025-02-04 00:13:01 +0900

    main First_Commit

---
+++ src/main/java/com/rolen/spring_train/SpringTrainApplication.java
@@ -0,0 +1,13 @@
+package com.rolen.spring_train;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTrainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rolen/spring_train/SpringTrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rolen.spring_train;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
